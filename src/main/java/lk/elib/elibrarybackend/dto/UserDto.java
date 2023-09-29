1ef0b60e8c513bc2381244a4b3b8aa5adaca703f
Pavan Silva <119662370+Pavan-Silva@users.noreply.github.com>
2023-09-29 11:54:42 +0530

    Set password to write-only in DTOs

--- src/main/java/lk/elib/elibrarybackend/dto/UserDto.java
+++ src/main/java/lk/elib/elibrarybackend/dto/UserDto.java
@@ -1,25 +1,23 @@
 package lk.elib.elibrarybackend.dto;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.Set;
-
 @Getter
 @Setter
 @NoArgsConstructor
 public class UserDto {
 
     Integer id;
-
-    @JsonIgnore
-    String username;
-
-    @JsonIgnore
-    String password;
-
     Byte active;
     Set<RoleDto> roles;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    String username;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    String password;
 }
